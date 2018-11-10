87325686a5290ff8a2f4f32296dcead0d202c104
Justin.B.Brown <brownee06@gmail.com>
2018-11-10 13:36:19 -0700

    Add connection to databse

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+
+//Define datbase to connect.
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection.
+module.exports = connection;
